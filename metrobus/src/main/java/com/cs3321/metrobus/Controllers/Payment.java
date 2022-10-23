288dd7a7f936a9c190995b49a68665a26e9f8dad
quangpnm92 <phamq16@gator.uhd.edu>
2022-10-23 16:30:08 -0500

    payment commit

---
+++ metrobus/src/main/java/com/cs3321/metrobus/Controllers/Payment.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cs3321.metrobus.Controllers;
+
+/**
+ *
+ * @author Quan
+ */
+public class Payment {
+    public boolean getPayment (String card, String name, String exp, String cvc)
+    {   
+        
+        return false;
+    }
+}

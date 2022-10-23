4f6fff9a012527703772d472cdcfb517100c4b35
quangpnm92 <phamq16@gator.uhd.edu>
2022-10-23 18:02:35 -0500

    committed Quang

---
+++ metrobus/src/main/java/com/cs3321/metrobus/View/ReceiptView.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cs3321.metrobus.View;
+import com.cs3321.metrobus.Controllers.Payment;
+/**
+ *
+ * @author Quan
+ */
+public class ReceiptView {
+    
+    public static void printReceipt(double price)
+    {
+        System.out.println("");
+    }
+}
